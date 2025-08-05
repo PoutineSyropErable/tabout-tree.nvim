aada0f8fc196a4575db49638a56be7e09d05b969
François Pagé <francois.page@mail.mcgill.ca>
2025-08-04 22:55:14 -0400

    upadte

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
+-- plugin/treesitter_inspect/init.lua
+local M = require("treesitter_inspect")
+
+M.setup = function() require("treesitter_inspect.keymaps").setup() end
+
+-- Optional: Auto-setup if user wants
 require("treesitter_inspect.keymaps").setup()
+
+return M
